619e2ab69ec56f9583d8ee565a9b4c02d2ca24d3
ayo1103 <ayo1103@gmail.com>
2016-02-21 22:31:17 +0800

    refactor

--- android7funs/app/src/main/kotlin/com/giantcroissant/sevenfuns/app/MainActivity.kt
+++ android7funs/app/src/main/kotlin/com/giantcroissant/sevenfuns/app/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.giantcroissant.sevenfuns.app
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +11,6 @@ import kotlinx.android.synthetic.main.toolbar.*
 
 
 class MainActivity : AppCompatActivity() {
-    val TAG = MainActivity::class.java.name
     val CURRENT_SECTION = "CURRENT_SECTION"
     var current_section = 0
 
@@ -20,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState != null) {
             current_section = savedInstanceState.getInt(CURRENT_SECTION);
-            Log.e(TAG, "onCreate -> savedInstanceState != null : current_section = $current_section")
 
         } else {
             current_section = R.id.navigationItemRecipesSection
@@ -32,60 +30,46 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        navigationView?.let {
-            it.setNavigationItemSelectedListener {
-                it.isChecked = true
-                drawerLayout?.closeDrawers()
+        navigationView?.setNavigationItemSelectedListener {
 
-                when (it.itemId) {
-                    R.id.navigationItemRecipesSection -> {
-                        current_section = R.id.navigationItemRecipesSection
-                        supportActionBar?.title = resources.getString(R.string.navigation_item_recipes_section)
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragmentContainer, RecipesSectionFragment.newInstance())
-                            .commit()
-                    }
-                    R.id.navigationItemPersonalSection -> {
-                        current_section = R.id.navigationItemPersonalSection
-                        supportActionBar?.title = resources.getString(R.string.navigation_item_personal_section)
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragmentContainer, PersonalSectionFragment.newInstance())
-                            .commit()
-                    }
-                    R.id.navigationItemInstructorSection -> {
-                        current_section = R.id.navigationItemInstructorSection
-                        supportActionBar?.title = resources.getString(R.string.navigation_item_instructor_section)
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragmentContainer, InstructorSectionFragment.newInstance())
-                            .commit()
-                    }
-                    R.id.navigationItemQASection -> {
-                        current_section = R.id.navigationItemQASection
-                        supportActionBar?.title = resources.getString(R.string.navigation_item_qa_section)
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragmentContainer, QASectionFragment.newInstance())
-                            .commit()
-                    }
-                    R.id.navigationItemSponsorSection -> {
-                        current_section = R.id.navigationItemSponsorSection
-                        supportActionBar?.title = resources.getString(R.string.navigation_item_sponsor_section)
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragmentContainer, SponsorSectionFragment.newInstance())
-                            .commit()
-                    }
+            it.isChecked = true
+            drawerLayout?.closeDrawers()
+
+            var fragment = Fragment()
+            when (it.itemId) {
+                R.id.navigationItemRecipesSection -> {
+                    current_section = R.id.navigationItemRecipesSection
+                    fragment = RecipesSectionFragment.newInstance()
                 }
-
-                true
+                R.id.navigationItemPersonalSection -> {
+                    current_section = R.id.navigationItemPersonalSection
+                    fragment = PersonalSectionFragment.newInstance()
+                }
+                R.id.navigationItemInstructorSection -> {
+                    current_section = R.id.navigationItemInstructorSection
+                    fragment = InstructorSectionFragment.newInstance()
+                }
+                R.id.navigationItemQASection -> {
+                    current_section = R.id.navigationItemQASection
+                    fragment = QASectionFragment.newInstance()
+                }
+                R.id.navigationItemSponsorSection -> {
+                    current_section = R.id.navigationItemSponsorSection
+                    fragment = SponsorSectionFragment.newInstance()
+                }
             }
 
-            it.setCheckedItem(current_section)
-            it.menu.performIdentifierAction(current_section, 0)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit()
+            true
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navigationView.setCheckedItem(current_section)
+        navigationView.menu.performIdentifierAction(current_section, 0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -95,27 +79,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        //        if (id == R.id.action_settings) {
-        //            return true
-        //        }
-
-        when (id) {
-            android.R.id.home -> drawerLayout?.openDrawer(GravityCompat.START)
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout?.openDrawer(GravityCompat.START)
+                return true
+            }
         }
-
         return super.onOptionsItemSelected(item)
     }
 
-
     override fun onSaveInstanceState(outState: Bundle?) {
-        Log.e(TAG, "onSaveInstanceState")
-
         outState?.putInt(CURRENT_SECTION, current_section);
 
         super.onSaveInstanceState(outState)
@@ -123,11 +96,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        Log.e(TAG, "onRestoreInstanceState")
 
         if (savedInstanceState != null) {
             current_section = savedInstanceState.getInt(CURRENT_SECTION);
-            Log.e(TAG, "onRestoreInstanceState -> savedInstanceState != null : current_section = $current_section")
 
         } else {
             current_section = R.id.navigationItemRecipesSection
